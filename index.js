39a43c8b9548f8dc3506014032b750c48207eea5
BEAU WITKA <bwitka@gmail.com>
2019-02-21 22:55:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ function createRock(x) {
          */
         if (checkCollision(rock)) {
             endGame();
-        } 
+        } else {
             let newTop = rock.offsetTop + 2;
             rock.style.top = newTop + "px";
             /**
@@ -105,7 +105,7 @@ function createRock(x) {
             } else {
               window.requestAnimationFrame(moveRock);
             }
-        
+        }
     }
 
     // We should kick of the animation of the rock around here
